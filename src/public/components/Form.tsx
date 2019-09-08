57727067e1b3a9e8725160256c6e605d6ae93ce2
umairnadeem123 <nadeem.umair123@gmail.com>
2019-09-07 21:58:35 -0700

    Complete Form superclass for generic type-checking

--- src/public/components/Form.tsx
+++ src/public/components/Form.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import R from 'ramda';
 
 interface Props<FormFields> {
-  Form: React.ComponentType<FormProps<FormFields>>;
+  FormComponent: React.ComponentType<FormProps<FormFields>>;
   initialValues: FormFields;
 }
 
@@ -13,6 +14,36 @@ interface Handlers<FormFields> {
   onChange: OnChangeHandler<FormFields>;
 };
 
-type OnChangeHandler<FormFields> = (
-  e: React.SyntheticEvent,
+type OnChangeHandler<FormFields> = <K extends keyof FormFields>(
+  event: React.ChangeEvent<HTMLInputElement>,
+  a?: FormFields[K],
 ) => void;
+
+interface State<FormFields> {
+  fields: FormFields;
+}
+
+class Form<FormFields> extends React.Component<
+  Props<FormFields>,
+  State<FormFields>
+> {
+  constructor(props: Props<FormFields>) {
+    super(props);
+    this.state = { fields: props.initialValues };
+  }
+
+  public onChange: OnChangeHandler<FormFields> = (event) => {
+    const { target: { name, value }} = event;
+    this.setState({ fields: R.merge(this.state.fields, { [name]: value }) });
+  }
+
+  public render() {
+    const { FormComponent } = this.props;
+    const { fields } = this.state;
+    return (
+      <FormComponent onChange={this.onChange} fields={fields} />
+    );
+  }
+}
+
+export default Form;
